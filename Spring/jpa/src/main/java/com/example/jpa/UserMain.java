677cdac79d272e6bcf9253ddd831694374d87129
hwan <hahaha1719@naver.com>
2024-05-27 17:55:53 +0900

    05/27 Commit

---
+++ Spring/jpa/src/main/java/com/example/jpa/UserMain.java
@@ -0,0 +1,32 @@
+package com.example.jpa;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
+public class UserMain {
+    //    private static final Logger log = LoggerFactory.getLogger(UserMain.class);
+    public static void main(String[] args) {
+        UserDAO userDAO = new UserDAO();
+        // findUser()
+        log.info("==========findUser()==========");
+        User finduser = userDAO.findUser(1L);
+        log.info("Found user : {}", finduser.getName());
+
+        User findUser2 = userDAO.findUser(1L);
+
+        if (finduser == findUser2) {
+            log.info("findUser == findUser2");
+        } else {
+            log.info("findUser != findUser2");
+        }
+//
+//        User hwanUser = new User("minji","minji@exam.com");
+//
+//        userDAO.createUser(hwanUser);
+//
+//        log.info("Create user : " + hwanUser.getName());
+//        log.info("user email : {}", hwanUser.getEmail());
+    }
+}
